344f8177469d247dd8695b8d3854c58c775ae04e
Matt <mjkern@andrew.cmu.edu>
2021-12-20 22:05:15 -0500

    be a good citizen of the internet

--- McLeanPres.py
+++ McLeanPres.py
@@ -5,13 +5,21 @@ class McLeanPres(Spider):
     Scrapes sermons from McLean Presbyterian Church, according to sermon series
     """
 
+    # basic settings
     name = "s0" # name of the spider
     allowed_domains = ["mcleanpres.org"]
     start_urls = ["https://mcleanpres.org/sermons/series/all"]
 
+    # don't abuse the website
+    custom_settings = {
+        'DOWNLOAD_DELAY': 0.5,
+        'DUPEFILTER_CLASS': 'scrapy.dupefilters.BaseDupeFilter'
+    }
+
     def parse(self, response):
         all_series = response.xpath("//div[@class='cell']/h4/a")
         results = []
+        print(len(all_series))
         for series in all_series:
             result = {}
             result['series_title'] = series.xpath("./text()").extract()
